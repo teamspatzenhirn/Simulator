a1f1590343a98468fbcd18c100753aca420b512b
Jona Ruof <jona.ruof@uni-ulm.de>
2018-08-10 12:53:12 +0200

    added modifier rendering in marker module

--- src/modules/MarkerModule.h
+++ src/modules/MarkerModule.h
@@ -6,6 +6,9 @@
 #include <memory>
 #include <algorithm>
 
+#include <cmath>
+#define _USE_MATH_DEFINES
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
@@ -13,16 +16,31 @@
 
 class MarkerModule {
 
+
     std::vector<glm::mat4*> modelMatrices;
 
     std::shared_ptr<Model> markerModel;
+    std::shared_ptr<Model> markerModelSelected;
 
+    std::shared_ptr<Model> arrowModel;
+    std::shared_ptr<Model> scaleArrowModel;
+    std::shared_ptr<Model> ringModel;
+
+    bool isClick;
+
+    glm::mat4* selectedMarker;
+    enum SelectionMode { TRANSLATE = 0, SCALE = 1, ROTATE = 2 } selectionMode;
+
+    void renderModifiers(
+            GLuint shaderProgramId,
+            std::shared_ptr<Model> model,
+            float scale, 
+            float offset);
 public:
 
     MarkerModule();
 
     void addMarker(glm::mat4& model);
-    void removeMarker(glm::mat4& model);
 
     void render(GLFWwindow* window, GLuint shaderProgramId, Camera& cameraMatrix);
 };
