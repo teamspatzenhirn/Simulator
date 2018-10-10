c29524e7859e90f654c3d48567baa29dda36393f
Jona Ruof <jona.ruof@uni-ulm.de>
2018-10-10 21:25:38 +0200

    added Id.h

---
+++ src/helpers/Id.h
@@ -0,0 +1,38 @@
+#ifndef INC_2019_ID_H
+#define INC_2019_ID_H
+
+#include <cstdint>
+
+/*
+ * This is a little fancy class definition that allows one to add a (reasonably)
+ * unique ID to any class/struct. This provides a better identification
+ * mechanism for objects then using e.g. memory addresses. 
+ */
+
+template<typename T> class Id : public T {
+
+    /*
+     * This has space for 18446744073709552615 IDs.
+     * Not joking. That should be enough.
+     */
+    static uint64_t LAST_ID;
+
+public:
+
+    /*
+     * This freaky statement, allows you to call the constructors
+     * of the base class T directly. Neat!
+     */
+    using T::T;
+
+    /*
+     * This is intentionally made const and not private with a
+     * getter. This way the id can still be changed in edge cases.
+     */
+    const uint64_t id = LAST_ID++;
+};
+
+template<typename T>
+uint64_t Id<T>::LAST_ID = 0;
+
+#endif
