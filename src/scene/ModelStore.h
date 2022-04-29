df36fe3d44be8749b9e2d3c222d3cc6144b10d0b
Dominik <dominik.authaler@uni-ulm.de>
2022-04-29 08:33:04 +0200

    replace magic number by last_element enum entry

--- src/scene/ModelStore.h
+++ src/scene/ModelStore.h
@@ -3,6 +3,8 @@
 
 #include "helpers/Model.h"
 
+#include "Scene.h"
+
 /*
  * Simple object, which holds and loads all used models
  * in the simulator. Should be used to avoid loading the
@@ -19,7 +21,7 @@ struct ModelStore {
     Model arrow;
     Model scaleArrow;
 
-    Model items[78];
+    Model items[ItemType::LAST_ELEMENT];
 
     ModelStore (std::string resPath) : 
         car{resPath + "models/spatz11.obj"},
